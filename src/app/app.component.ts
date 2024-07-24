6412588dfeaab66e1576fa2e587873782df9770b
Chihab Otmani <chihab@gmail.com>
2024-07-24 16:51:50 +0100

    feat: experiments with databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UsersComponent } from './core/users/users.component';
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [UsersComponent, NgIf, NgStyle, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sofrecom-july';
+  users = [
+    { id: 1, firstName: 'user1', age: 20 },
+    { id: 2, firstName: 'user2', age: 30 },
+    { id: 3, firstName: 'user3', age: 40 },
+  ];
+  counter = 0;
+  isLoading = false;
+  buttonStyle = { color: 'red' };
+  inputClass = 'error';
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'after 2s';
+    }, 2000);
+  }
+
+  onClick() {
+    this.counter = this.counter + 1;
+    this.buttonStyle = { color: 'blue' };
+    this.inputClass = 'success';
+  }
+
+  getCounter() {
+    console.log('getCounter called');
+    return this.counter;
+  }
 }
---
+++ src/app/core/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { UserComponent } from '../user/user.component';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [UserComponent],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css',
+})
+export class UsersComponent {}
